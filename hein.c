7c295c8df11cc6b4cba2e28a67601077ccb5fa0a
adisaikat <adi.saikat1805@gmail.com>
2026-01-01 23:37:57 +0530

    day 12

--- hein.c
+++ hein.c
@@ -44,6 +44,7 @@ struct editorConfig {
   int screencols;
   int numrows;
   erow *row;
+  int pos;
   struct termios orig_termios;
 };
 
@@ -218,12 +219,12 @@ void editorMoveCursor(int key) {
   switch (key) {
   case ARROW_LEFT:
     if (E.cx != 0) {
-      E.cx--;
+      E.pos = --E.cx;
     }
     break;
   case ARROW_RIGHT:
     if (row && E.cx < row->size) {
-      E.cx++;
+      E.pos = ++E.cx;
     }
     break;
   case ARROW_UP:
@@ -236,6 +237,13 @@ void editorMoveCursor(int key) {
       E.cy++;
     }
     break;
+  }
+
+  row = (E.cy >= E.numrows) ? NULL : &E.row[E.cy];
+  if (row) {
+    E.cx = (E.pos > row->size) ? row->size : E.pos;
+  } else {
+    E.cx = 0;
   }
 }
 
@@ -377,6 +385,7 @@ void editorRefreshScreen() {
 void initEditor() {
   E.cx = 0;
   E.cy = 0;
+  E.pos = 0;
   E.rowoff = 0;
   E.coloff = 0;
   E.numrows = 0;
