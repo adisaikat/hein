7d762cf4eb0282ed82b84943cb8ac05e064a391c
adisaikat <adi.saikat1805@gmail.com>
2026-01-09 22:20:21 +0530

    day 16

--- hein.c
+++ hein.c
@@ -56,6 +56,7 @@ struct editorConfig {
   char statusmsg[80];
   time_t statusmsg_time;
   struct termios orig_termios;
+  // TODO: (raw and inset modes universal var)
 };
 
 struct editorConfig E;
@@ -245,6 +246,26 @@ void editorAppendRow(char *s, size_t len) {
   E.numrows++;
 }
 
+void editorRowInsertChar(erow *row, int at, int c) {
+  if (at < 0 || at > row->size)
+    at = row->size;
+  row->chars = realloc(row->chars, row->size + 2);
+  memmove(&row->chars[at + 1], &row->chars[at], row->size - at + 1);
+  row->size++;
+  row->chars[at] = c;
+  editorUpdateRow(row);
+}
+
+/*** editor operations ***/
+
+void editorInsertChar(int c) {
+  if (E.cy == E.numrows) {
+    editorAppendRow("", 0);
+  }
+  editorRowInsertChar(&E.row[E.cy], E.cx, c);
+  E.cx++;
+}
+
 /*** file i/o ***/
 
 void editorOpen(const char *filename) {
@@ -364,6 +385,12 @@ void editorProcessKeypress() {
   case ARROW_RIGHT:
     editorMoveCursor(c);
     break;
+
+    // TODO: (raw and inset modes)
+
+  default:
+    editorInsertChar(c);
+    break;
   }
 }
 
@@ -479,7 +506,7 @@ void editorDrawMessageBar(struct abuf *ab) {
   int msglen = strlen(E.statusmsg);
   if (msglen > E.screencols)
     msglen = E.screencols;
-  if(msglen && time(NULL) - E.statusmsg_time < 5){
+  if (msglen && time(NULL) - E.statusmsg_time < 5) {
     abAppend(ab, E.statusmsg, msglen);
   }
 }
